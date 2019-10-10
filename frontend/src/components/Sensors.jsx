f33c0d3f445dc647abca28a8f2b6966120d414a5
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2019-10-10 18:06:55 +0300

    pressure

--- frontend/src/components/Sensors.jsx
+++ frontend/src/components/Sensors.jsx
@@ -26,6 +26,7 @@ class Sensors extends Component {
     // bgTemp = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'];
     bgTemp = ['rgba(170,99,99,0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 0.2)'];
     bgHumidity = ['rgba(59,93,170,0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)'];
+    bgPressure = ['rgba(255, 159, 64, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 159, 64, 0.2)'];
     borderColors = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
 
     constructor() {
@@ -38,7 +39,7 @@ class Sensors extends Component {
     }
 
     getData() {
-        let datasets_temperature = [], datasets_humidity = [], sensorIDs = [1, 3, 4, 5];
+        let datasets_temperature = [], datasets_humidity = [], datasets_pressure = [], sensorIDs = [1, 3, 4, 5];
         fetch('http://' + window.location.hostname + ':3000/sensors').then((res) => {
             return res.json();
         }).then((sensors) => {
@@ -53,13 +54,23 @@ class Sensors extends Component {
                     };
 
                     datasets_humidity[i] = {
-                        label: 'p' + i.toString(),
+                        label: 'h' + i.toString(),
                         data: sensors.filter(r => r.sensorID === sensorIDs[i]).map(r => r.humidity),
                         backgroundColor: this.bgHumidity[i],
                         borderColor: this.borderColors[1],
                         borderWidth: 0.1,
                         pointRadius: 1.0
+                    };
+
+                    datasets_pressure[i] = {
+                        label: 'p' + i.toString(),
+                        data: sensors.filter(r => r.sensorID === sensorIDs[i]).map(r => r.pressure),
+                        backgroundColor: this.bgPressure[i],
+                        borderColor: this.borderColors[2],
+                        borderWidth: 0.1,
+                        pointRadius: 1.0
                     }
+
                 }
                 console.log(datasets_temperature);
                 this.setState({
@@ -92,7 +103,23 @@ class Sensors extends Component {
                                 }
 
                             }),
+                        },
+                    data_pressure:
+                        {
+                            datasets: datasets_pressure,
+                            labels: sensors.filter(r => {
+                                return r.sensorID === 1;
+                            }).map(r => {
+                                let d = new Date(r.createdAt);
+                                if (d.getMinutes() < 10) {
+                                    return d.getHours() + ':0' + d.getMinutes();
+                                } else {
+                                    return d.getHours() + ':' + d.getMinutes();
+                                }
+
+                            }),
                         }
+
                 });
                 console.log(this.state);
             }
@@ -111,10 +138,20 @@ class Sensors extends Component {
                           height={300}
                           width={500}/>
                 </div>
+
                 <p>Humidity</p>
                 <div>
                     <Line id={'humidity'}
                           data={this.state.data_humidity}
+                          options={{maintainAspectRatio: false}}
+                          height={300}
+                          width={500}/>
+                </div>
+
+                <p>Pressure</p>
+                <div>
+                    <Line id={'pressure'}
+                          data={this.state.data_pressure}
                           options={{maintainAspectRatio: false}}
                           height={300}
                           width={500}/>

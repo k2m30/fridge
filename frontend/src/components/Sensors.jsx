b74c9a1a58d17d1609a25a590eea90ddcb7b60af
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2019-10-10 17:44:53 +0300

    humidity

--- frontend/src/components/Sensors.jsx
+++ frontend/src/components/Sensors.jsx
@@ -23,6 +23,19 @@ class Sensors extends Component {
         }
     };
 
+    // bgTemp = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'];
+    bgTemp = ['rgba(145,79,79,0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 0.2)'];
+    bgHumidity = ['rgba(45,66,125,0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)'];
+
+    borderColors = [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ];
+
     constructor() {
         super();
         this.getData();
@@ -33,40 +46,47 @@ class Sensors extends Component {
     }
 
     getData() {
-        let datasets = [], sensorIDs = [1, 3, 4, 5];
+        let datasets_temperature = [], datasets_humidity = [], sensorIDs = [1, 3, 4, 5];
         fetch('http://' + window.location.hostname + ':3000/sensors').then((res) => {
             return res.json();
         }).then((sensors) => {
                 for (let i = 0; i < 4; i++) {
-                    datasets[i] = {
+                    datasets_temperature[i] = {
                         label: 't' + i.toString(),
-                        data: sensors.filter(r => {
-                            return r.sensorID === sensorIDs[i];
-                        }).map(r => r.temperature),
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
+                        data: sensors.filter(r => r.sensorID === sensorIDs[i]).map(r => r.temperature),
+                        backgroundColor: this.bgTemp[i],
+                        borderColor: this.borderColors[0],
+                        borderWidth: 0.2
+                    };
+
+                    datasets_humidity[i] = {
+                        label: 'p' + i.toString(),
+                        data: sensors.filter(r => r.sensorID === sensorIDs[i]).map(r => r.humidity),
+                        backgroundColor: this.bgHumidity[i],
+                        borderColor: this.borderColors[1],
+                        borderWidth: 0.1
                     }
                 }
-                console.log(datasets);
+                console.log(datasets_temperature);
                 this.setState({
-                    data:
+                    data_temperature:
                         {
-                            datasets: datasets,
+                            datasets: datasets_temperature,
+                            labels: sensors.filter(r => {
+                                return r.sensorID === 1;
+                            }).map(r => {
+                                let d = new Date(r.createdAt);
+                                if (d.getMinutes() < 10) {
+                                    return d.getHours() + ':0' + d.getMinutes();
+                                } else {
+                                    return d.getHours() + ':' + d.getMinutes();
+                                }
+
+                            }),
+                        },
+                    data_humidity:
+                        {
+                            datasets: datasets_humidity,
                             labels: sensors.filter(r => {
                                 return r.sensorID === 1;
                             }).map(r => {
@@ -89,12 +109,24 @@ class Sensors extends Component {
     render() {
         return (
             <div>
-                <Line data={this.state.data}
-                      options={{maintainAspectRatio: false}}
-                      height={300}
-                      width={500}/>
-                <div><p></p></div>
+                <p>Temperature</p>
+                <div>
+                    <Line id={'temperature'}
+                          data={this.state.data_temperature}
+                          options={{maintainAspectRatio: false}}
+                          height={300}
+                          width={500}/>
+                </div>
+                <p>Humidity</p>
+                <div>
+                    <Line id={'humidity'}
+                          data={this.state.data_humidity}
+                          options={{maintainAspectRatio: false}}
+                          height={300}
+                          width={500}/>
+                </div>
             </div>
+
         )
     }
 }

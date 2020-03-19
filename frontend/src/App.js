2ec80414e2bacbebd4bf127efd1b2932a4469df2
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2020-03-19 17:20:49 +0300

    state view added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Sensors from "./components/Sensors";
 import Controls from "./components/Controls";
+import State from "./components/State";
 
 class App extends Component {
     render() {
@@ -12,6 +13,7 @@ class App extends Component {
                     Prosciutto di hackerspace
                 </h2>
                 <div><Sensors/></div>
+                <div><State/></div>
                 <div><Controls/></div>
             </div>
 
---
+++ frontend/src/components/State.jsx
@@ -0,0 +1,130 @@
+import React, {Component} from 'react';
+import './Sensors.css';
+import {defaults, Line} from 'react-chartjs-2';
+
+defaults.global.animation = false;
+
+
+class State extends Component {
+    state = {
+        data: {
+            labels: [],
+            datasets: []
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            maintainAspectRatio: false
+        }
+    };
+
+    // bgTemp = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'];
+    bgTemp = ['rgba(170,99,99,0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 0.2)'];
+    bgHumidity = ['rgba(59,93,170,0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)'];
+    borderColors = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
+
+    constructor() {
+        super();
+        this.getData();
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.getData(), 30000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    getData() {
+        let datasets_temperature = [], datasets_humidity = [];
+        fetch('http://' + window.location.hostname + ':3000/state').then((res) => {
+            return res.json();
+        }).then((state) => {
+
+                    datasets_temperature = [{
+                        label: 't',
+                        data: state.map(r => r.t),
+                        backgroundColor: this.bgTemp[0],
+                        borderColor: this.borderColors[0],
+                        borderWidth: 0.2,
+                        pointRadius: 1.0
+                    }];
+
+                    datasets_humidity = [{
+                        label: 'h',
+                        data: state.map(r => r.h),
+                        backgroundColor: this.bgHumidity[0],
+                        borderColor: this.borderColors[1],
+                        borderWidth: 0.1,
+                        pointRadius: 1.0
+                    }];
+
+
+                console.log(datasets_temperature);
+                this.setState({
+                    data_temperature:
+                        {
+                            datasets: datasets_temperature,
+                            labels: state.map(r => {
+                                let d = new Date(r.createdAt);
+                                if (d.getMinutes() < 10) {
+                                    return d.getHours() + ':0' + d.getMinutes();
+                                } else {
+                                    return d.getHours() + ':' + d.getMinutes();
+                                }
+
+                            }),
+                        },
+                    data_humidity:
+                        {
+                            datasets: datasets_humidity,
+                            labels: state.map(r => {
+                                let d = new Date(r.createdAt);
+                                if (d.getMinutes() < 10) {
+                                    return d.getHours() + ':0' + d.getMinutes();
+                                } else {
+                                    return d.getHours() + ':' + d.getMinutes();
+                                }
+
+                            }),
+                        },
+                });
+                console.log(this.state);
+            }
+        );
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Temperature</p>
+                <div>
+                    <Line id={'temperature'}
+                          data={this.state.data_temperature}
+                          options={{maintainAspectRatio: false}}
+                          height={300}
+                          width={500}/>
+                </div>
+
+                <p>Humidity</p>
+                <div>
+                    <Line id={'humidity'}
+                          data={this.state.data_humidity}
+                          options={{maintainAspectRatio: false}}
+                          height={300}
+                          width={500}/>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default State;
